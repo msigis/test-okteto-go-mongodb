89a6041ffa228704e8ebad1bdae4b69b9a60e188
msigis <gianmarco.sigismondi@gmail.com>
2021-05-27 21:02:34 +0200

    add http CLient

--- go.mod
+++ go.mod
@@ -3,25 +3,14 @@ module myfood-app
 go 1.16
 
 require (
-	github.com/0xAX/notificator v0.0.0-20191016112426-3962a5ea8da1 // indirect
-	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
-	github.com/codegangsta/gin v0.0.0-20171026143024-cafe2ce98974 // indirect
-	github.com/gin-gonic/gin v1.7.1 // indirect
-	github.com/go-playground/validator/v10 v10.6.1 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/json-iterator/go v1.1.11 // indirect
-	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/mattn/go-shellwords v1.0.11 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/ugorji/go v1.2.6 // indirect
-	go.mongodb.org/mongo-driver v1.5.2 // indirect
+	github.com/aws/aws-sdk-go v1.38.47 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/klauspost/compress v1.12.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	go.mongodb.org/mongo-driver v1.5.2
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
-	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/text v0.3.6 // indirect
-	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
-	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
-	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,23 @@
-
 package main
 
 import (
+	"bufio"
 	"context"
 	"encoding/json"
 	"fmt"
-//	"log"
+
+	//	"log"
+	"net/http"
+	"time"
+
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"net/http"
-	"time"
 )
 
- var client *mongo.Client
+var client *mongo.Client
 
 type Food struct {
 	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
@@ -23,11 +25,34 @@ type Food struct {
 	Tribe string             `json:"tribe,omitempty" bson:"tribe,omitempty"`
 }
 
+func TestFood(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	resp, err := http.Get("http://gobyexample.com")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	response.WriteHeader(http.StatusAccepted)
+	//fmt.Println("Response status:", resp.Status)
+	response.Write([]byte(`{ "reponseClient": "` + resp.Status + `" }`))
+
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		fmt.Println(scanner.Text())
+		response.Write([]byte(`{ "messageClient": "` + scanner.Text() + `" }`))
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+}
+
 func AddFood(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("content-type", "application/json")
 	var food Food
 	_ = json.NewDecoder(request.Body).Decode(&food)
-fmt.Println(food.Name)
+	fmt.Println(food.Name)
 	collection := client.Database("myfood").Collection("foods")
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	result, _ := collection.InsertOne(ctx, food)
@@ -79,23 +104,26 @@ func GetFoods(response http.ResponseWriter, request *http.Request) {
 
 func main() {
 	fmt.Println("Starting the application on port 8080")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017")
-	 client, _  = mongo.Connect(ctx, clientOptions)
-	// Connect to MongoDB
-//	if err != nil {
-//		log.Fatal(err)
-//	}	
-//	err = client.Ping(context.TODO(), nil)
 
-//	if err != nil {
-  //  		log.Fatal(err)
-	
-//	}	
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	//clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017")
+	client, _ = mongo.Connect(ctx, clientOptions)
+	// Connect to MongoDB
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	err = client.Ping(context.TODO(), nil)
+
+	//	if err != nil {
+	//  		log.Fatal(err)
+
+	//	}
 	fmt.Println("Connectd to MondoDB")
-	
+
 	router := mux.NewRouter()
 	router.HandleFunc("/food", AddFood).Methods("POST")
+	router.HandleFunc("/test", TestFood).Methods("POST")
 	router.HandleFunc("/food", GetFoods).Methods("GET")
 	router.HandleFunc("/food/{id}", GetFood).Methods("GET")
 	http.ListenAndServe(":8080", router)
